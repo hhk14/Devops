b66bd8e1b764947eaf98718f523e4aa326c7bd35
Harisha H K <harishahk875@gmail.com>
2022-08-03 12:33:43 +0000

    Resolved conlict issue

--- ar_ops_on_two_nos.sh
+++ ar_ops_on_two_nos.sh
@@ -7,10 +7,9 @@ echo "Enter the second number"
 read num2
 sum=0
 sum=$(( $num1 + $num2))
-<<<<<<< HEAD
-echo "the sum of $num1 and $num2 is :"
-echo "$sum"
-=======
+
+#echo "the sum of $num1 and $num2 is :"
+#echo "$sum"
+
 echo " Sum of two numbers is:$sum"
->>>>>>> subbranch1
 
