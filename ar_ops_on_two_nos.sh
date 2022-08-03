1595852e2adb122196dd14e4d476a97253deba55
Harisha H K <harishahk875@gmail.com>
2022-08-03 13:08:10 +0000

    Included product calculation in ar_ops_on_two_nos.sh file

--- ar_ops_on_two_nos.sh
+++ ar_ops_on_two_nos.sh
@@ -7,5 +7,8 @@ echo "Enter the second number"
 read num2
 sum=0
 sum=$(( $num1 + $num2))
+product=1
+product=$(( $product * $num1 * $num2))
 echo "Sum of two numbers is:$sum"
+echo "Product of two numbers is : $product"
 
